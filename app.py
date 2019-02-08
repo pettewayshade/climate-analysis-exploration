94193746c199639edef57427cc8b31deb92f477e
ShadeP <pettewayshade@gmail.com>
2019-02-08 12:40:19 -0600

    Completed fucntions for last 2 routes

--- app.py
+++ app.py
@@ -28,13 +28,13 @@ app = Flask(__name__)
 def home():
     return (
         f"Available Routes:<br/>"
-        f'-- /api/v1.0/precipitation<br/>'
-        f'-- /api/v1.0/stations<br/>'
-        f'-- /api/v1.0/tobs</br>'
+        f'-- /api/v1.0/precipitation<br/><br/>'
+        f'-- /api/v1.0/stations<br/><br/>'
+        f'-- /api/v1.0/tobs<br/><br/>'
         f'-- /api/v1.0/start<br/>'
-        f'Note: Start date must be between 2010-01-01 and 2017-08-23<br/>'
+        f'Note: Start date must be between 2010-01-01 and 2017-08-23<br/><br/>'
         f'-- /api/v1.0/start/end<br/>'
-        f'Note: Start date cannot be sooner thatn 2010-01-01 and end date cannot be later than 2017-08-23'
+        f'Note: Start date cannot be sooner that 2010-01-01 and end date cannot be later than 2017-08-23'
     )
 
 @app.route('/api/v1.0/precipitation')
@@ -46,7 +46,9 @@ def percipitation():
     
     prcp = dict(prcp)
     
+    str = 'Perceipation results for previous year:'
     return jsonify(prcp)
+
 
 @app.route('/api/v1.0/stations')
 def stations():
@@ -55,28 +57,76 @@ def stations():
     
     allStations = list(np.ravel(results))
     
-    return jsonify(allStations)
+    msg = f'List of all stations from the dataset:'
+    
+    return jsonify(f'{msg} {allStations}')
+
 
 @app.route('/api/v1.0/tobs')
 def tobs():
     gdate = dt.datetime(2017, 8, 18) - dt.timedelta(days=365)
     tobs = session.query(Measurement.tobs).\
         filter(Measurement.date >= gdate).\
-        order_by(Measurement.tobs.desc()).all()
+        order_by(Measurement.tobs.desc()).\
+        group_by(Measurement.tobs).all()
     session.close()
     
     tobs = list(np.ravel(tobs))
     
-    msg = f'List of Temperature Obeservations (tobs) for the previous year'
-    return jsonify(tobs)
+    msg = f'List of Temperature Obeservations (tobs) for the previous year:'
+    
+    return jsonify(f'{msg} {tobs}')
+
 
 @app.route('/api/v1.0/<start>')
-def startdate():
-    return f'No info'
+def startdate(start):
+    if start < '2010-01-01' or start > '2017-08-23': 
+        return jsonify({"error": f"Start date: {start} is not within range."}), 404
+    
+    results = session.query(
+             func.min(Measurement.tobs),
+             func.max(Measurement.tobs),
+             func.avg(Measurement.tobs)).\
+             filter(Measurement.date >= start).all()
+    session.close()
+    
+    results = list(np.ravel(results))
+    
+    item_dict = {
+        'Min tob': results[0],
+        'Max tob': results[1],
+        'Avg tob': results[2]
+        }
+        
+    msg = f'TOB results for dates greater than or equal to {start}:'
+    
+    return jsonify(f'{msg} {item_dict}')
+
 
 @app.route('/api/v1.0/<start>/<end>')
-def startenddate():
-    return f'No info'
+def startenddate(start,end):
+    if start < '2010-01-01' or end  > '2017-08-23' or start >= end or end <= start: 
+        return jsonify({"error": f"Start date: {start} or end date: {end} is not within range."}), 404
+    
+    results = session.query(
+             func.min(Measurement.tobs),
+             func.max(Measurement.tobs),
+             func.avg(Measurement.tobs)).\
+             filter(Measurement.date >= start, Measurement.date <= end).all()
+    session.close()
+    
+    results = list(np.ravel(results))
+    
+    item_dict = {
+        'Min tob': results[0],
+        'Max tob': results[1],
+        'Avg tob': results[2]
+        }
+    
+    msg = f'TOB results for date range {start} - {end}:'
+    
+    return jsonify(f'{msg} {item_dict}')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
